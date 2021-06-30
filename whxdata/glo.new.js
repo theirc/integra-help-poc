44a5017a8d2d24f6f22344ba29dd5553560bd50e
nealrogers-theirc <72216695+nealrogers-theirc@users.noreply.github.com>
2021-06-30 11:01:58 -0400

    Added glossary to output

---
+++ whxdata/glo.new.js
@@ -0,0 +1,4 @@
+(function() {
+var glossary =  {"type":"glossary","chunkinfos":[{"type":"chunkinfo","first":"Accounts Receivable","last":"Master Service Agreement (MSA)","num":"45","node":"gdata1"}]};
+window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), glossary, { sync:true });
+})();

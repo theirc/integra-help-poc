9b63524ec8743b939c72892f9b1643ccf3b4095b
nealrogers-theirc <72216695+nealrogers-theirc@users.noreply.github.com>
2021-06-30 13:28:33 -0400

    Preview update

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("Key_Components_of_Inventory_Management.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '52744c92-3620-4474-b6f6-8fdf68cf95fd' || 'preview');
+rh.consts('HELP_ID', '454a2ed2-1957-43b8-b75b-02a400aaf1af' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "en");

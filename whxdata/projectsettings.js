2a50d02effa99a880bce860e14038054a4980237
nealrogers-theirc <72216695+nealrogers-theirc@users.noreply.github.com>
2021-07-08 10:39:51 -0400

    Receive, Quality Inspection, Dispatch, Transfers, Returns import

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("Accounts_Receivable.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', 'b656cacc-d609-421b-a8c0-1975a14a6216' || 'preview');
+rh.consts('HELP_ID', '4e827b6f-95fb-4331-afb9-bb84f39c743b' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "en");

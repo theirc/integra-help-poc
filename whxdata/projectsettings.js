44b05c4f20dbd82fdbb85c2b5350400fb959181a
nealrogers-theirc <72216695+nealrogers-theirc@users.noreply.github.com>
2021-07-09 14:00:12 -0400

    Unordered list fix

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("Personalization_Overview.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', 'e437a7ff-5c06-45c9-97e6-e113b6d6eccf' || 'preview');
+rh.consts('HELP_ID', '54e56b0d-1357-4d70-8bd2-bc3ad889e74b' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "en");

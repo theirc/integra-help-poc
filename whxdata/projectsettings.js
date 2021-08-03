a2f60bbd3f8385bf2da558d92191a0739957470d
nealrogers-theirc <72216695+nealrogers-theirc@users.noreply.github.com>
2021-08-03 08:59:44 -0400

    Restored remove-highlight button in functionbar

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("Personalization_Overview.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '1590f996-9fbc-4196-b9ad-0c1c8eb85457' || 'preview');
+rh.consts('HELP_ID', '71e37e4c-45e5-4662-9384-3d64f2e2813b' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "en");

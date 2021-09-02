59a6f050180fdf08e1d63a2580edc57f7e99a9b2
ElijahRooney <elijah.rooney@rescue.org>
2021-09-02 14:22:54 -0400

    Removed all underscores from ToC

--- whxdata/glo.new.js
+++ whxdata/glo.new.js
@@ -1,4 +1,4 @@
 (function() {
-var glossary =  {"type":"glossary","chunkinfos":[{"type":"chunkinfo","first":"Accounts Receivable","last":"Master Service Agreement (MSA)","num":"45","node":"gdata1"}]};
+var glossary =  {"type":"glossary","chunkinfos":[{"type":"chunkinfo","first":"Accounts Receivable","last":"XDS (Extensible Data Security) ","num":"91","node":"gdata1"}]};
 window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), glossary, { sync:true });
 })();
--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("About_Integra.htm#About_Integra"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '690bd10a-78ca-42ba-8a72-d78ef4900bd4' || 'preview');
+rh.consts('HELP_ID', '6d09c6b5-6c0b-4fb3-bb5f-62cab896c54c' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "en");

a1c0b250b97c4c33c7ea9d453c48feba7066effb
ElijahRooney <72215807+ElijahRooney@users.noreply.github.com>
2022-07-28 17:20:14 -0400

    Seeing If I can add the links back to the DEV site

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("About_Integra.htm#About_Integra"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', 'd51d9465-cae8-4b7b-9f65-67e6be60de8f' || 'preview');
+rh.consts('HELP_ID', 'e9eaf019-ea96-4211-8272-e066870e7fbc' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "en");

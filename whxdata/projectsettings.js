d249c28ca4257594301b57d235125c89057f4af8
ElijahRooney <72215807+ElijahRooney@users.noreply.github.com>
2022-12-13 11:01:36 -0500

    Shannon updated the ToC

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("About_Integra.htm#About_Integra"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '3fd0cf9c-0c38-4a0f-92e1-b14ba04c50bf' || 'preview');
+rh.consts('HELP_ID', '1d02adb8-4f26-4495-a360-3462c61d285c' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "en");
